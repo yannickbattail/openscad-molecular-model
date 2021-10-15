6994d47d215c9f47ea80ff4059bdd6beefe33110
yannick <yannick.battail@raisepartner.com>
2021-10-15 16:33:06 +0200

    update generators: add  bond_1part bond_short and Dimethylhydrazine

--- generate_images.sh
+++ generate_images.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ./images/
 
-pieces_list="all Hydrogen Chlorine Oxygen Sulfur Nitrogen Carbon bond_end bond Alcohol Methane Ammonia Carbon_dioxide cyanide Alkane Cyclohexane 22dimethylpropane"
+pieces_list="all Hydrogen Chlorine Oxygen Sulfur Nitrogen Carbon bond_end bond bond_1part bond_short Alcohol Methane Ammonia Carbon_dioxide cyanide Alkane Cyclohexane 22dimethylpropane Dimethylhydrazine"
 for piece in $pieces_list
 do
     echo generating $piece ...
