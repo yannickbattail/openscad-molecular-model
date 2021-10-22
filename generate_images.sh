315321f6f80986e5948e23eab944e15041399ce9
yannick <yannick.battail@raisepartner.com>
2021-10-22 19:31:17 +0200

    bigger images

--- generate_images.sh
+++ generate_images.sh
@@ -6,5 +6,5 @@ pieces_list="all Hydrogen Chlorine Oxygen Sulfur Nitrogen Carbon bond_end bond b
 for piece in $pieces_list
 do
     echo generating $piece ...
-    openscad -q -o ./images/${piece}.png -D "piece=\"$piece\"" ./main.scad
+    openscad -q -o ./images/${piece}.png -D "piece=\"$piece\"" --imgsize 1024,1024 ./main.scad
 done
