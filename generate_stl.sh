d96bf7739c2e596a055a529368e82a70ddd3d17c
yannick <yannick.battail@raisepartner.com>
2021-11-09 14:29:34 +0100

    regenerate images and stl

--- generate_stl.sh
+++ generate_stl.sh
@@ -7,7 +7,7 @@ pieces_list="Hydrogen Oxygen Nitrogen Carbon bond_end bond_1part bond_short"
 for piece in $pieces_list
 do
     echo generating $piece ...
-    openscad -q -o ./stl/${piece}.stl -D "piece=\"$piece\"" ./main.scad
+    openscad -q -o ./stl/${piece}.stl -D "piece=\"$piece\"" -D "bond_pin_with_clip=false" ./main.scad
     echo generating $piece with clip ...
-    openscad -q -o ./stl/${piece}_with_clip.stl -D "piece=\"$piece\"" -D "bond_pin_with_clip=true" ./main.scad
+    openscad -q -o ./stl/${piece}_with_clip.stl -D "piece=\"$piece\"" ./main.scad
 done
